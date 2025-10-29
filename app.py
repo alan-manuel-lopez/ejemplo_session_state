b04910fe546433147b81e7b224ff6d2806b5bdff
alan-manuel-lopez <a377341@uach.mx>
2025-10-29 08:21:51 -0700

    Initialize session state for name and count

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("Ejemplo para usar session_state")
 
+if 'name' not in st.session_state:
+    st.session_state['name']=''
+    
 #si la etiqueta "count" no existe en la session state, se agregara la etiqueta "count" con un valor
 if 'count' not in st.session_state:
     st.session_state['count'] = 0
-
-if 'name' not in st.session_state:
-    st.session_state['name']=''
 
 if st.button('Click me'):
     st.session_state['count']+=1
